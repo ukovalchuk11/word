0cd18aa0d1f39f11072f26117ca59e00bed0f923
uriy <pirat2000@ukr.net>
2018-03-19 21:34:50 +0200

    github

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Word");
         System.out.println("Goodbye Word");
+	System.out.println("Word!");
     }
 }
